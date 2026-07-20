8ffc0dfd5fbc43d4d74fedd130fbd5d45180fb2d
Abi-coder007 <abishekcoder2007@gmail.com>
2026-07-20 22:15:04 +0530

    Add files via upload

---
+++ sets2.py
@@ -0,0 +1,34 @@
+# Union
+
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+results = set1.union(set2)
+print(results)
+
+
+# Intersection
+
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+results = set1.intersection(set2)
+print(results)
+
+
+# Difference
+
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+results = set1.difference(set2)
+print(results)
+
+# Symmetric Difference
+
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+results = set1.symmetric_difference(set2)
+print(results)
+
